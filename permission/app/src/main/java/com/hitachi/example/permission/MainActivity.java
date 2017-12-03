17754d9f56cf539df9a6ea5310c379984d2cb353
Hitachi <mac@hitachikerker.local>
2017-12-03 19:42:35 +0800

    [Hitachi] Test how to request android permission

---
+++ permission/app/src/main/java/com/hitachi/example/permission/MainActivity.java
@@ -0,0 +1,33 @@
+package com.hitachi.example.permission;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                REQUEST_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                if (grantResults.length <= 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+}
